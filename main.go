6cf06035ac0fd2e33e92f5e3865676c8aa52e951
QuinnDK <18207205276@163.com>
2020-04-27 15:08:31 +0800

    继续加油

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -33,5 +34,25 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%s", result)
+	//fmt.Printf("%s", result)
+	ParseContent(result)
+
+	//正则表达式
+	//	str:="asdajidasfksaiaioaskfivsvs"
+	//	re:=regexp.MustCompile(".*ksai")
+	//	rest:=re.FindString(str)
+	//	fmt.Println(rest)
+}
+
+func ParseContent(content []byte) {
+	//<a href="/tag/哲学" class="tag">哲学</a>
+	//re:=regexp.MustCompile(`<a href="([^"]+)">([^</a>]+)</a>`)
+	re := regexp.MustCompile(`<a href="([^"]+)" class="tag">([^"]+)</a>`) //外边的小点 ` 并非是引号，我靠，花了好长时间
+	matches := re.FindAllSubmatch(content, -1)
+
+	for _, m := range matches {
+		fmt.Printf("url:%s\n", "https://book.douban.com"+string(m[1]))
+
+	}
+
 }
