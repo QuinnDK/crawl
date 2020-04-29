7e2ffe4748560868a7f4accc5a10647b6471fdb0
QuinnDK <18207205276@163.com>
2020-04-29 12:24:05 +0800

    艰难学习中，明日开学，湖北加油

---
+++ engine/engine.go
@@ -0,0 +1,29 @@
+package engine
+
+import (
+	"crawl/fetcher"
+	"fmt"
+	"log"
+)
+
+func Run(seeds ...Request) {
+	var requests []Request
+	for _, e := range seeds {
+		requests = append(requests, e)
+	}
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+		log.Printf("Fetching url:%s", r.Url)
+		body, err := fetcher.Fetch(r.Url)
+		if err != nil {
+			log.Printf("Fetching error:%s", r.Url)
+		}
+		parseResult := r.ParseFunc(body)
+		requests = append(requests, parseResult.Requesrts...)
+
+		for _, item := range parseResult.Items {
+			fmt.Printf("Go Item:%s\n", item)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,44 +1,16 @@
 package main
 
 import (
-	"crawl/fetcher"
+	"crawl/engine"
 	"crawl/parse"
 )
 
 func main() {
-	////resp,err:=http.Get("https://book.douban.com/top250?start=0")  豆瓣已经加入的发爬虫机制，这种方法不能用了
-	//res, err := http.NewRequest("GET", "https://book.douban.com", nil)
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
-	//res.Header.Add("User-Agent", "test")
-	//
-	//client := &http.Client{}
-	//resp, err2 := client.Do(res)
-	//
-	//if err2 != nil {
-	//	err = err2
-	//	return
-	//}
-	//defer resp.Body.Close() //延迟关闭
-	//
-	//if resp.StatusCode != http.StatusOK { //statusOK 值为两百，像404等等为服务器异常
-	//	fmt.Printf("Error status code : %d", resp.StatusCode)
-	//
-	//}
-	//result, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	panic(err)
-	//}
+	engine.Run(engine.Request{
+		Url:       "https://book.douban.com",
+		ParseFunc: parse.ParseContent,
+	})
+	//result, _ := fetcher.Fetch("https://book.douban.com")
+	//parse.ParseContent(result)
 
-	//fmt.Printf("%s", result)
-	result, _ := fetcher.Fetch("https://book.douban.com")
-	parse.ParseContent(result)
-
-	//正则表达式
-	//	str:="asdajidasfksaiaioaskfivsvs"
-	//	re:=regexp.MustCompile(".*ksai")
-	//	rest:=re.FindString(str)
-	//	fmt.Println(rest)
 }
