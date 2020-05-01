8523b7cfd287d1ecf6d48ed4c87bbf5e3effe110
QuinnDK <18207205276@163.com>
2020-05-01 10:27:39 +0800

    五一快乐，隔离一时爽，一直隔离一直爽

--- model/bookdetails.go
+++ model/bookdetails.go
@@ -1,10 +1,17 @@
 package model
 
+import "strconv"
+
 type Bookdetail struct {
+	Title     string
 	Author    string
 	Public    string
 	Booktages int
 	Price     string
 	Score     string
 	Into      string
+}
+
+func (b Bookdetail) String() string {
+	return "书名: " + b.Title + " 作者: " + b.Author + " 出版社:" + b.Public + " 页数: " + strconv.Itoa(b.Booktages) + "  价格: " + b.Price + " 评分:" + b.Score + " 内容简介:" + b.Into
 }
--- parse/parsebookdetail.go
+++ parse/parsebookdetail.go
@@ -7,10 +7,11 @@ import (
 	"strconv"
 )
 
+var title = regexp.MustCompile(`<span property="v:itemreviewed">([^<]+)</span>`)
 var autoRe = regexp.MustCompile(`<span class="pl"> 作者</span>:[\d\D]*?<a.*?>([^<]+)</a>`)
-var publuc = regexp.MustCompile(`<span class="pl">出版社:</span>([^<]+)<br>`)
-var pageRe = regexp.MustCompile(`<span class="pl">页数:</span> ([^<]+)<br>`)
-var priceRe = regexp.MustCompile(`<span class="pl">定价:</span> ([^<]+ )<br>`)
+var publuc = regexp.MustCompile(`<span class="pl">出版社:</span>([^<]+)<br/>`)
+var pageRe = regexp.MustCompile(`<span class="pl">页数:</span> ([^<]+)<br/>`)
+var priceRe = regexp.MustCompile(`<span class="pl">定价:</span> ([^<]+)<br/>`)
 var scoreRe = regexp.MustCompile(`<strong class="ll rating_num " property="v:average">([^<]+ )</strong>`)
 var intoRe = regexp.MustCompile(`<div class="intro">[\d\D]*?<p>([^<]+)</p>[\d\D]*?<p>([^<]+)</p></div>`)
 
@@ -18,6 +19,7 @@ func ParseBookDetail(contents []byte) engine.ParseResult {
 
 	bookedetail := model.Bookdetail{}
 
+	bookedetail.Title = ExtraString(contents, title)
 	bookedetail.Author = ExtraString(contents, autoRe)
 	bookedetail.Public = ExtraString(contents, publuc)
 	page, err := strconv.Atoi(ExtraString(contents, pageRe))
