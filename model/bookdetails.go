036688db90f7dd10136d49a1402ed2a0b9292936
QuinnDK <18207205276@163.com>
2020-05-02 09:04:16 +0800

    五一快乐，隔离一时爽，一直隔离一直爽

--- model/bookdetails.go
+++ model/bookdetails.go
@@ -13,5 +13,5 @@ type Bookdetail struct {
 }
 
 func (b Bookdetail) String() string {
-	return "书名: " + b.Title + " 作者: " + b.Author + " 出版社:" + b.Public + " 页数: " + strconv.Itoa(b.Booktages) + "  价格: " + b.Price + " 评分:" + b.Score + " \n内容简介:" + b.Into
+	return "书名: " + b.Title + " 作者: " + b.Author + " 出版社:" + b.Public + " 页数: " + strconv.Itoa(b.Booktages) + "  价格: " + b.Price + " 评分:" + b.Score + "\n内容简介:" + b.Into
 }
