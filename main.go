554601b665fe587acde6fc5a8092ab90df06ece3
QuinnDK <18207205276@163.com>
2021-01-13 20:53:58 +0800

    last

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"crawl/LearnGo-crawl/crawl_distribute/client"
 	"crawl/LearnGo-crawl/engine"
-	"crawl/LearnGo-crawl/persist"
+	//"crawl/LearnGo-crawl/persist"
 
+	client2 "crawl/LearnGo-crawl/crawl_distribute/work/client"
 	//"crawl/parse"
 	"crawl/LearnGo-crawl/parse/zhenai"
 	"crawl/LearnGo-crawl/scheduler"
 )
 
 func main() {
-	itemsave, err := persist.ItemSave()
+	//itemsave, err := persist.ItemSave()
+	itemsave, err := client.ItemSave("1234")
+	process, err := client2.CreateProcess()
 	if err != nil {
 		panic(err)
 	}
@@ -18,10 +22,11 @@ func main() {
 		&scheduler.QueueScheluder{},
 		100,
 		itemsave,
+		process,
 	}
 	e.Run(engine.Request{
-		Url:       "http://www.zhenai.com/zhenghun",
-		ParseFunc: zhenai.ParseCity,
+		Url:   "http://www.zhenai.com/zhenghun",
+		Parse: engine.NewFuncparse(zhenai.ParseCityList, "ParseCityList"),
 	})
 
 	//result, _ := fetcher.Fetch("https://book.douban.com")
